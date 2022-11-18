1fbe9f031922c06eded01a1535ddb70510a0748b
salvafern <salvador.fernandez@vliz.be>
2022-11-18 14:50:36 +0100

    Add binder infrastructure

---
+++ install.R
@@ -0,0 +1,4 @@
+install.packages("RNetCDF", version = "2.6.1")
+install.packages("readr", version = "2.1.2")
+install.packages("dplyr", version = "1.0.9")
+install.packages("glue", version = "1.6.2")
